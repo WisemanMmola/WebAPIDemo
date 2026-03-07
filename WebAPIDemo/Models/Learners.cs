74f654c4002bf4c1b18903273565e57888d3d0c2
WisemanMmola <mmolaecommerce11@gmail.com>
2026-03-08 00:35:59 +0200

    Added Learners.cs model

---
+++ WebAPIDemo/Models/Learners.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPIDemo.Models
+{
+
+    //Learners information
+    public class Learners
+    {
+        public Guid Id { get; set; }
+        public required string LearnerName { get; set; }
+        public required string LearnerSurname { get; set; }
+        public required string LearnerEmail { get; set; }
+        public string? LearnerPhone { get; set; }
+        public required string LearnerIdNumber { get; set; }
+    }
+}
