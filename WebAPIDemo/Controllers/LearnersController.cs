9eda0178f202890c2107b0a32fba9cb5482c915c
WisemanMmola <mmolaecommerce11@gmail.com>
2026-03-25 22:07:38 +0200

    Added a LearnersController

---
+++ WebAPIDemo/Controllers/LearnersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Data;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("/api/controller")]
+    [ApiController]
+    public class LearnersController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public LearnersController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var Mylearner = _context.learners.ToList();
+            if (Mylearner is null)
+            {
+                return NotFound("No learners where found");
+            }
+            return Ok(Mylearner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddLearner(Learners Learner)
+        {
+           await _context.learners.AddAsync(Learner);
+            await _context.SaveChangesAsync();
+            return Ok(Learner);
+        }
+
+    }
+}
